c0969365a1e3f132a8fb4f0f8f0dfa8892dca768
jevgeni.tsupov@ttu.ee <slayer666>
2019-08-07 11:14:36 +0300

    first

---
+++ target/classes/database.sql
@@ -0,0 +1,48 @@
+# -- Table: users
+# CREATE TABLE users (
+#                        id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#                        username VARCHAR(255) NOT NULL,
+#                        password VARCHAR(255) NOT NULL
+# )
+#     ENGINE = InnoDB;
+#
+# -- Table: roles
+# CREATE TABLE roles (
+#                        id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#                        name VARCHAR(100) NOT NULL
+# )
+#     ENGINE = InnoDB;
+#
+# -- Table for mapping user and roles: user_roles
+# CREATE TABLE user_roles (
+#                             user_id INT NOT NULL,
+#                             role_id INT NOT NULL,
+#
+#                             FOREIGN KEY (user_id) REFERENCES users(id),
+#                             FOREIGN KEY (role_id) REFERENCES roles (id),
+#
+#                             UNIQUE (user_id, role_id)
+# )
+#     ENGINE = InnoDB;
+#
+# INSERT INTO users VALUES (1, 'test', 'test');
+#
+# INSERT INTO roles VALUES (1, 'ROLE_USER');
+# INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
+#
+# INSERT INTO user_roles VALUES (1, 2);
+#
+
+CREATE TABLE products (
+                          id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          name     VARCHAR(255) NOT NULL,
+                          quantity INT          NOT NULL ,
+                          price    DOUBLE          NOT NULL
+)
+    ENGINE = InnoDB;
+
+INSERT INTO products VALUES (1,'Milk',5,1.20);
+INSERT INTO products VALUES (2,'Water',15,0.99);
+
+
+drop table products;
